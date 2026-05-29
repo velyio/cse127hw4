ac271753a4fe2e90f8cc2c68e51718c4040c76e4
yvcardenas <147003383+yvcardenas@users.noreply.github.com>
2026-05-28 18:25:31 -0700

    part4

---
+++ bleichenbacher.py
@@ -0,0 +1,48 @@
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+from roots import *
+import sys
+
+# Takes message as a command line argument
+message = sys.argv[1]
+
+# Your code to forge a signature goes here.
+# Load the public key and extract n and e
+publickey = RSA.importKey(open('tnb_key.pub').read())
+n = publickey.n #
+e = publickey.e # the exponent, should be 3
+
+# Hash the message using SHA-1
+h = SHA.new(message.encode())
+digest = h.digest()
+
+# Make the fake padded message
+msg = bytearray()
+msg.append(0x00)
+msg.append(0x01)
+
+# Does check the number of 0xFF bytes so just chose 10
+msg.extend(b'\xFF'*10)
+msg.append(0x00)
+# ASN.1 "magic" bytes for SHA-1
+msg.extend(b'\x30\x21\x30\x09\x06\x05\x2b\x0e\x03\x02\x1a\x05\x00\x04\x14')
+# 20-byte SHA-1 digest
+msg.extend(digest)  
+
+# Pad the rest of the message with zeros
+msg.extend(b'\00' * (256 - len(msg)))
+
+padded_message = bytes(msg)
+
+# Take the cube root of the padded message
+# turn bytes to integer using function from root.py
+m = bytes_to_integer(padded_message)
+
+s, is_exact = integer_nthroot(m, e)
+
+# turn the integer signature back to bytes
+print(integer_to_base64(s).decode())
+
+# some example functions from roots
+# root, is_exact = integer_nthroot(27, 3)
+# print(integer_to_base64(root).decode())
